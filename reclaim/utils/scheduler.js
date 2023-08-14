586dfbea0e1e9eba58b7519c90e4d32a250149ec
Kushal7788 <kushshah777888@gmail.com>
2023-08-14 20:13:44 +0530

    updated timeout

--- reclaim/utils/scheduler.js
+++ reclaim/utils/scheduler.js
@@ -31,7 +31,7 @@ const checkMember = async (member, guild, role) => {
 const iterateMembers = async (guild) => {
     try {
         const guildConfig = await GuildConfig.findOne({ guildId: guild.id });
-        if (guildConfig && (Date.now() - guildConfig.data.scheduledStartTime > (guildConfig.data.scheduledTimer - 30000))) {
+        if (guildConfig) {
             guildConfig.data = {
                 ...guildConfig.data,
                 scheduledStartTime: Date.now()
