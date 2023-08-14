8c7336c6a611c4d5cd2f1a82c2671e3e3f644b9b
Kushal7788 <kushshah777888@gmail.com>
2023-08-14 21:09:20 +0530

    scheduler freq

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function delay(ms) {
 client.on('ready', async () => {
     await delay(3000);
     console.log('Bot is ready.');
-    const schedulerTimer = 1000 * 60 * 60 * 6; // 6 hours
+    const schedulerTimer = 1000 * 60 * 60 * 3; // 6 hours
     // Schedule the role check and message sending
     // await checkAllVerificationStatus();
     try {
